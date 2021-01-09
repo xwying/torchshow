801c49c1767ea83f9719a4008e66961270bd999f
Xiaowen <sslf@foxmail.com>
2021-01-08 22:07:42 -0500

    update license

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Visualizing tensor data in one line of code.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/xwying/tensorshow",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
