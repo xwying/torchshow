ef396b5d776dcb14ec35fe215d66098721abc342
Xiaowen <sslf@foxmail.com>
2021-01-08 21:50:22 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tensorshow", # Replace with your own username
-    version="0.0.1",
+    version="0.2.0",
     author="Xiaowen Ying",
     author_email="xiaowenying93@gmail.com",
-    description="Quickly visualize pytorch tensors.",
+    description="Visualizing tensor data in one line of code.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
