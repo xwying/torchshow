879d2837aee040b5b1ce6c1bc6e9925022dbff75
Xiaowen <sslf@foxmail.com>
2022-05-18 23:09:29 -0400

    Fix a bug where binary mask will be infered as category mask. Write some more tests.

--- tests/tests.py
+++ tests/tests.py
@@ -1,36 +1,50 @@
 import torchshow as ts
 import torch 
+import logging
 
 ts.use_inline(False)
 
-test_img = torch.rand((3, 100, 100)) * 50
-# ts.show(test_img)
+print("1.1 Single 3-channel image between 0-1")
+rgb_img = torch.rand((3, 100, 100))
+print(rgb_img.min(), rgb_img.max())
+ts.show(rgb_img)
 
-test_grayscale = torch.rand((1, 100, 100))
-# ts.show(test_grayscale)
+print("1.2 Single 3-channel image between 0-255")
+rgb_img_2 = rgb_img * 100
+print(rgb_img_2.min(), rgb_img_2.max())
+ts.show(rgb_img_2)
 
-test_category_mask = torch.randint(-1, 25, (1, 10, 10))
-# ts.show(test_category_mask)
+print("1.3 Single 3-channel image with value larger than 255")
+rgb_img_3 = rgb_img * 300
+print(rgb_img_3.min(), rgb_img_3.max())
+ts.show(rgb_img_3)
 
-test_flow = torch.rand((2, 100, 100))
-# ts.headless(True)
-ts.save(test_flow)
-ts.save(test_flow, './test_fig/vis/test.png')
+print("1.4 Single 3-channel image with value smaller than 0")
+rgb_img_4 = rgb_img - 0.5
+print(rgb_img_4.min(), rgb_img_4.max())
+ts.show(rgb_img_4)
 
-# feature_maps = torch.rand((64, 32, 32))
+print("1.5 Single 3-channel image with value smaller than 0 and larger than 255")
+rgb_img_5 = rgb_img * 500 - 100
+print(rgb_img_5.min(), rgb_img_5.max())
+ts.show(rgb_img_5)
 
-# ts.show([[test_img, test_grayscale], [test_category_mask]])
-# ts.show(feature_maps)
+print("2.1 Single 1-Channel image between 0-1")
+gray_img = torch.rand((1, 100, 100))
+print(gray_img.min(), gray_img.max())
+ts.show(gray_img)
 
-# ts.show(feature_maps, nrows=4)
+print("2.2 Single 1-Channel image between 0-255")
+gray_img_2 = gray_img * 100
+print(gray_img_2.min(), gray_img_2.max())
+ts.show(gray_img_2)
 
-# ts.show(feature_maps, title_pattern="channel {img_id_from_1}")
-# ts.show(test_grayscale)
-# ts.show(test_category_mask)
+print("2.3 Single 1-Channel image with value smaller than 0 and larger than 255")
+gray_img_3 = gray_img *  500 - 100
+print(gray_img_3.min(), gray_img_3.max())
+ts.show(gray_img_3)
 
-# video1 = torch.rand((8, 3, 100, 100))
-# video2 = torch.rand((15, 3, 100, 100))
-
-# ts.show_video(video2)
-# ts.show(video1)
-# ts.show_video([[video1, video2], [video1, video2]])
+print("2.4 Single 1-Channel image with binary value")
+gray_img_4 = torch.randint(0, 2, (1, 100, 100))
+print(gray_img_4.unique())
+ts.show(gray_img_4)
