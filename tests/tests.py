f87a7556ddb5a46635d52e6ea79d08ba6da79dfe
Xiaowen <sslf@foxmail.com>
2022-06-15 14:48:53 -0400

    Adapt same API to ts.save()

--- tests/tests.py
+++ tests/tests.py
@@ -27,26 +27,31 @@ def test(section):
         rgb_img = torch.rand((3, 100, 100))
         print(rgb_img.min(), rgb_img.max())
         ts.show(rgb_img)
+        ts.save(rgb_img)
 
         print("1.2 Single 3-channel image between 0-255")
         rgb_img_2 = rgb_img * 100
         print(rgb_img_2.min(), rgb_img_2.max())
         ts.show(rgb_img_2)
+        ts.save(rgb_img_2)
 
         print("1.3 Single 3-channel image with value larger than 255")
         rgb_img_3 = rgb_img * 300
         print(rgb_img_3.min(), rgb_img_3.max())
         ts.show(rgb_img_3)
+        ts.save(rgb_img_3)
 
         print("1.4 Single 3-channel image with value smaller than 0")
         rgb_img_4 = rgb_img - 0.5
         print(rgb_img_4.min(), rgb_img_4.max())
         ts.show(rgb_img_4)
+        ts.save(rgb_img_4)
 
         print("1.5 Single 3-channel image with value smaller than 0 and larger than 255")
         rgb_img_5 = rgb_img * 500 - 100
         print(rgb_img_5.min(), rgb_img_5.max())
         ts.show(rgb_img_5)
+        ts.save(rgb_img_5)
 
     gray_img = torch.rand((1, 100, 100))
     category_mask = np.array(Image.open('test_data/example_category_mask.png'))
@@ -54,16 +59,19 @@ def test(section):
         print("2.1 Single 1-Channel image between 0-1")
         print(gray_img.min(), gray_img.max())
         ts.show(gray_img)
+        ts.save(gray_img)
 
         print("2.2 Single 1-Channel image between 0-255")
         gray_img_2 = gray_img * 100
         print(gray_img_2.min(), gray_img_2.max())
         ts.show(gray_img_2)
+        ts.save(gray_img_2)
 
         print("2.3 Single 1-Channel image with value smaller than 0 and larger than 255")
         gray_img_3 = gray_img *  500 - 100
         print(gray_img_3.min(), gray_img_3.max())
         ts.show(gray_img_3)
+        ts.save(gray_img_3)
 
         print("2.4 Single 1-Channel image with binary value")
         gray_img_4 = torch.eye(100).unsqueeze(0)
@@ -71,54 +79,68 @@ def test(section):
         gray_img_4[:, 60:80, 60:80] = 1 
         print(gray_img_4.unique())
         ts.show(gray_img_4)
+        ts.save(gray_img_4)
 
         print("2.5 Single 1-Channel image with integer value >= 0")
         gray_img_5 = torch.randint(0, 100, (1, 100, 100))
         print(gray_img_5.unique())
         ts.show(gray_img_5)
+        ts.save(gray_img_5)
 
         print("2.6 Single 1-Channel image with both positive and negative integer value")
         gray_img_6 = torch.randint(-50, 100, (1, 100, 100))
         print(gray_img_6.unique())
         ts.show(gray_img_6)
+        ts.save(gray_img_6)
         
         print("2.7 Single 1-Channel normal categorical mask")
         print(np.unique(category_mask))
         ts.show(category_mask)
+        ts.save(category_mask)
 
     flow = read_flow("./test_data/example_flow.flo")
     if section <= 3:
         print("3.1 Single 2-Channel normal optical flow")
         print(flow.shape, flow.min(), flow.max())
         ts.show(flow)
+        ts.save(flow)
 
         print("3.2 Single 2-Channel with random values")
         flow_2 = torch.rand((2, 100, 100)) * 200 - 100
         print(flow_2.min(), flow_2.max())
         ts.show(flow_2)
+        ts.save(flow_2)
 
     img_n = torch.rand(16, 100, 100)
     if section <=4:
         print("4.1 Single n-channel tensor (n>3")
         print(img_n.min(), img_n.max())
         ts.show(img_n)
+        ts.save(img_n)
         ts.show(img_n, ncols=3)
+        ts.save(img_n, ncols=3)
         ts.show(img_n, nrows=5)
+        ts.save(img_n, nrows=5)
 
         print("4.2 4D tensors")
         batch = torch.rand(8, 3, 100, 100) * 500 - 250
         print(batch.min(), batch.max())
         ts.show(batch)
+        ts.save(batch)
         ts.show(batch, ncols=3)
+        ts.save(batch, ncols=3)
         ts.show(batch, nrows=4)
+        ts.save(batch, nrows=4)
 
     if section <=5:
         print("5.1 Custom Layout")
         grid = [[rgb_img, gray_img, flow]]
         ts.show(grid)
+        ts.save(grid)
         grid2 = [[rgb_img, gray_img], 
                  [flow]]
         ts.show(grid2)
+        ts.save(grid2)
 
     if section <=6:
         print("6.1 Video Clip")
